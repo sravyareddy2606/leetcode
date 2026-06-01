74a715918ba3a940d05b899f49c58a940c2e5a6d
koulagarisravyareddy <koulagarisravyareddy@gmail.com>
2026-06-01 16:55:34 +0530

    Time: 135 ms (87.86%), Space: 31.8 MB (71.96%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums)<2:
+            return 0
+        nums.sort()
+        max_gap=0
+        for i in range(1,len(nums)):
+            diff=nums[i]-nums[i-1]
+            if diff>max_gap:
+                max_gap=diff
+        return max_gap
+        
