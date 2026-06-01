14c616ef4ae3e76f046dca7258b4b12394892125
koulagarisravyareddy <koulagarisravyareddy@gmail.com>
2026-06-01 16:46:48 +0530

    Time: 61 ms (5.2%), Space: 20.6 MB (12.36%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calculateMinimumHP(self, a: List[List[int]]) -> int:
+        R, C = len(a), len(a[0])
+        hi = [sum([r for r in row if r<0]+[0]) for row in a]
+        if not hi:return 1
+        else: hi = sum(hi)
+        lo, hi = 1, -hi+1
+        def dp(mid):
+            dp = [[-float('inf')]*(C+1) for _ in range(R+1)]
+            dp[1][1] = mid + a[0][0]
+            if dp[1][1]<=0:return False
+            for i in range(1, R+1):
+                for j in range(1, C+1):
+                    this_val = max(dp[i][j-1], dp[i-1][j])+a[i-1][j-1]
+                    if this_val > 0:dp[i][j] = this_val
+            return dp[R][C]>0                           
+        while lo < hi:
+            mid = (lo+hi)//2
+            if dp(mid):
+                hi = mid
+            else:
+                lo = mid+1
+        return lo
